b341309a0f2c1ab437a8cb041a7ebdb3886290b2
Santiago <149838863+santiagocorr3a@users.noreply.github.com>
2025-03-18 11:04:37 -0500

    preparing for render test

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -87,7 +89,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person);
 
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
